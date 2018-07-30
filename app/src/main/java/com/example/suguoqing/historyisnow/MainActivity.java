52bb30204322ed6a0b23281436815b861df6aa48
suguoqing <suguoqing@szba-mobile.com>
2018-07-30 16:30:35 +0800

    将导航栏基本实现
    Signed-off-by: suguoqing <suguoqing@szba-mobile.com>

--- app/src/main/java/com/example/suguoqing/historyisnow/MainActivity.java
+++ app/src/main/java/com/example/suguoqing/historyisnow/MainActivity.java
@@ -13,11 +13,18 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private static final String TAG = "MainActivity";
@@ -38,6 +45,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         materialCalendarView = findViewById(R.id.calendar_view);
         drawerLayout = findViewById(R.id.drawer_layout);
         FloatingActionButton btn = findViewById(R.id.float_btn);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        View drawerView = navigationView.inflateHeaderView(R.layout.nav_header);//把头部加进去
+        //实现头部监听
+        CircleImageView circleImageView = drawerView.findViewById(R.id.icon_image);
+        TextView username = drawerView.findViewById(R.id.username);
+        TextView email = drawerView.findViewById(R.id.email);
+        circleImageView.setOnClickListener(this);
+        username.setOnClickListener(this);
+        email.setOnClickListener(this);
+
+
         btn.setOnClickListener(this);
 
         //设置ToolBar
@@ -65,6 +83,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             }
         });
+        //默认选中
+        Date date = new Date();
+        materialCalendarView.setSelectedDate(date);
+
+
+        //监听nav_view中的内容
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_share:
+                        Toast.makeText(MainActivity.this, "share", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_shin:
+                        Toast.makeText(MainActivity.this, "shin", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_setting:
+                        Toast.makeText(MainActivity.this, "shin", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_about:
+                        Toast.makeText(MainActivity.this, "shin", Toast.LENGTH_SHORT).show();
+                        break;
+                        default:
+                            break;
+                }
+                return true;
+            }
+        });
 
 
 
@@ -86,8 +132,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()){
             case R.id.float_btn:
                 Toast.makeText(this, "hahah  dian ji wo ", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.icon_image:
+                Toast.makeText(this, "icom_image", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.username:
+                Toast.makeText(this, "username", Toast.LENGTH_SHORT).show();
+                break;
 
+            case R.id.email:
+                Toast.makeText(this, "email ", Toast.LENGTH_SHORT).show();
+                break;
+
+            default:
                 break;
         }
     }
+
+
+
 }
